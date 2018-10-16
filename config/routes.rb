e422c70f8ef1427ddbfacab564360e0876fcbbb0
Mac <mac@MacBook-Pro.local>
2018-10-16 16:51:45 -0500

    added splashpage, needs styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     registration: 'users/registration',
     sessions: 'users/sessions'
   }
+  root to: 'static#splashpage'
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
