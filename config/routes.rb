d2f068d63bcfafdaceba76ccb499570b08f31cc7
richard trapanese <richardtrapanese@gmail.com>
2018-10-16 17:15:08 -0400

    created route_maps and email_models view and controllers. lightly styled with BS4

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     registration: 'users/registration',
     sessions: 'users/sessions'
   }
+  resources :email_models
+  resources :route_maps
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
