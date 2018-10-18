2ef1e5ba45028646eb9ef3b5980ab2fc3a2574b4
Garth Puckerin <garth.puckerin@gmail.com>
2018-10-18 14:05:38 -0400

    Fixed profile routing in creation and updates

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,49 @@
 class ProfilesController < ApplicationController
   def index
+    @profiles = Profile.all
+  end
+
+  def show
+    @profile = Profile.find(params[:id])
   end
 
   def edit
+    @profile = Profile.find(params[:id])
   end
 
   def update
+    profile = Profile.find(params[:id])
+    profile.update(
+      first_name: params[:profile][:first_name],
+      last_name: params[:profile][:last_name],
+      phone: params[:profile][:phone],
+      address1: params[:profile][:address1],
+      address2: params[:profile][:addresse2],
+      city: params[:profile][:city],
+      state: params[:profile][:state],
+      zipcode: params[:profile][:zipcode]
+    )
+
+    redirect_to profile_path(profile)
+  end
+
+  def create
+    profile = Profile.create(
+      first_name: params[:profile][:first_name],
+      last_name: params[:profile][:last_name],
+      phone: params[:profile][:phone],
+      address1: params[:profile][:address1],
+      address2: params[:profile][:addresse2],
+      city: params[:profile][:city],
+      state: params[:profile][:state],
+      zipcode: params[:profile][:zipcode],
+      user_id: current_user.id
+    )
+
+    redirect_to locations_new_path
+  end
+
+  def new
+    @profile = Profile.new
   end
 end
