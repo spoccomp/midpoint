a03784a169c75b4b389f90b64a0c52610b3820eb
Garth Puckerin <garth.puckerin@gmail.com>
2018-10-22 14:32:59 -0400

    Updated Favorite controller methods

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,34 @@
 class FavoritesController < ApplicationController
   def index
+    @favorites = Favorite.all
   end
 
   def show
+    @favorite
   end
 
   def create
+    favorite = Favorite.create(
+      name: params[:student][:name],
+      address: params[:student][:address],
+      lat: params[:student][:lat],
+      long: params[:student][:long],
+      type: params[:favorite][:type]
+      user_id: params[][]
+    )
+
   end
 
   def new
+    @favorite = Favorite.new
   end
 
   def destroy
+    @favorite.destroy
+  end
+
+  private
+  def favorite
+    @favorite = Favorite.find(params[:id])
   end
 end
